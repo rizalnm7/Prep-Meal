5d3b87854f009bb288b7fd71d95c4229fd93348e
rizalnm7 <lazir07@gmail.com>
2026-06-19 15:31:08 +0700

    Update SUPABASE_ANON_KEY in config.js

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@
 // asal RLS policy di schema.sql sudah dijalankan.
 // ============================================================
 const SUPABASE_URL = "https://vftaofnwvefubsjgzymh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_secret_ttopTEyb-huGuPGD8U-hXA_9K5nwwoZ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdGFvZm53dmVmdWJzamd6eW1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NDkxODcsImV4cCI6MjA5NzQyNTE4N30.g2kcXwR3OhiaKmLhYiilv456PpGBd1pdCDil69SmPGw";
